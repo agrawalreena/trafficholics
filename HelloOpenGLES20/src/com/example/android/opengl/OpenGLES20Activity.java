395291575a6c5627d84aa8bd81f13e1e0a8134bf
Reena Agrawal <babli.agrawal@gmail.com>
2014-07-14 21:29:30 +0530

    Making activity full screen

--- HelloOpenGLES20/src/com/example/android/opengl/OpenGLES20Activity.java
+++ HelloOpenGLES20/src/com/example/android/opengl/OpenGLES20Activity.java
@@ -18,6 +18,8 @@ package com.example.android.opengl;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class OpenGLES20Activity extends Activity {
 
@@ -26,6 +28,10 @@ public class OpenGLES20Activity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         // Create a GLSurfaceView instance and set it
         // as the ContentView for this Activity
